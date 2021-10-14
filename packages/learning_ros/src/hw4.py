e776c68beb74de671f2b25929b28478f719912ba
amanda <Amanda_Taylor1@student.uml.edu>
2021-10-13 22:24:20 -0400

    updates

--- packages/learning_ros/src/hw4.py
+++ packages/learning_ros/src/hw4.py
@@ -5,8 +5,8 @@ from std_msgs.msg import Float32
 
 class MysteryNode:
     def __init__(self):
-        rospy.Subscriber("output2", Float32, self.callback)
-        self.pub_units = rospy.Publisher("conversion", Float32, queue_size=10)
+        rospy.Subscriber("conversion", Float32, self.callback)
+        self.pub_units = rospy.Publisher("input", Float32, queue_size=10)
 
     def callback(self, msg):
         self.pub_msg = msg
@@ -15,14 +15,17 @@ class MysteryNode:
         if unit == "feet":
           self.pub_msg.data = self.pub_msg.data * 3.28
           self.pub_units.publish(self.pub_msg)
+          rospy.loginfo("feet")
           
         elif unit == "meters":
           self.pub_msg.data = self.pub_msg.data
           self.pub_units.publish(self.pub_msg)
+          rospy.loginfo("meters")
           
         else:
            self.pub_msg.data = self.pub_msg.data * 0.588
-           self.pub_units.publish(self.pub_msg)       
+           self.pub_units.publish(self.pub_msg)
+           rospy.loginfo("smoots")       
         
 if __name__ == '__main__':
     rospy.init_node('mystery_node')
