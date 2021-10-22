281d7bf65c693d476fc3a287f4579c42e5d4ac54
amanda <Amanda_Taylor1@student.uml.edu>
2021-10-21 22:47:30 -0400

    final

--- packages/learning_ros/src/hw5.py
+++ packages/learning_ros/src/hw5.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from math import radians, sin, cos
+from math import radians, sin, cos, sqrt
 import numpy
 import rospy
 from duckietown_msgs.msg import Vector2D
 
 class TransformNode:
-    def __init__(self, ...):
+    def __init__(self):
           self.R = rospy.Publisher("robot", Vector2D, queue_size=10)
           self.W = rospy.Publisher("world", Vector2D, queue_size=10)
           R_Px = 2            #X-position of Robot
@@ -15,16 +15,16 @@ class TransformNode:
           R_Px_s = -2
           R_Py_s = 0
           self.R_T_s = numpy.matrix([[-1,0,-2],
-                                [0,-1,0]
+                                [0,-1,0],
                                 [0,0,1]])
-          self.W_T_R = numpy.matrix([[-1/sqrt(2),-1/sqrt(2),2]
-                                [1/sqrt(2),-1/sqrt(2),7]
+          self.W_T_R = numpy.matrix([[-1/sqrt(2),-1/sqrt(2),2],
+                                [1/sqrt(2),-1/sqrt(2),7],
                                 [0,0,1]])
-          self.W_T_S = W_T_R*R_T_s
+          self.W_T_S = self.W_T_R*self.R_T_s
           rospy.Subscriber("transform", Vector2D, self.callback_function)
           
     def callback_function(self, msg):
-          s_p = numpy.matrix([[msg.x] ,[msg.y],[1]])
+          s_p = numpy.matrix([[msg.x],[msg.y],[1]])
           robot_p = self.R_T_s*s_p
           world_p = self.W_T_R*robot_p
                            
