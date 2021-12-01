57b9ab7869d07cb1ec47badd9756b0e4ec6be792
amanda <Amanda_Taylor1@student.uml.edu>
2021-11-30 20:59:27 -0500

    update

--- packages/lab_2/src/line.py
+++ packages/lab_2/src/line.py
@@ -5,33 +5,32 @@ from duckietown_msgs.msg import Twist2DStamped, FSMState
 
 class ShapeNode:
     def __init__(self):
-          self.pub = rospy.Publisher('lane_controller_node/car_cmd', Twist2DStamped, queue_size=10)
-          self.sub = rospy.Subscriber('fsm_node/mode', FSMState, queue_size=10)
-          self.flag = False
+        self.pub = rospy.Publisher('lane_controller_node/car_cmd', Twist2DStamped, queue_size=10)
+        self.sub = rospy.Subscriber('fsm_node/mode', FSMState, self.callback_function)
+        self.flag = False
                         
     def callback_function(self, msg):      
-          fwd = Twist2DStamped()
-          if msg.state == "LANE_FOLLOWING" and self.flag == False:
-          	self.flag = True
-          	time = rospy.get_time()
-          	while rospy.get_time() < time + 1.9:
-          		fwd.v = 0.8
-          		fwd.omega = -0.5
-          		self.pub.publish(V)
-          		rospy.sleep(0.2)
-          	fwd.v = 0
-          	fwd.omega = 0
-          	self.pub.publish(V)          	
-          elif msg.state == "JOYSTICK_CONTROL"
-          	fwd.v = 0
-          	fwd.omega = 0
-         	self.flag = False
+        fwd = Twist2DStamped()
+        if msg.state == "LANE_FOLLOWING" and self.flag == False:
+            self.flag = True
+            time = rospy.get_time()
+            while rospy.get_time() < time + 2.3:
+                fwd.v = 0.8
+                fwd.omega = -0.8
+                self.pub.publish(fwd)
+                rospy.sleep(0.1)
+            fwd.v = 0
+            fwd.omega = 0
+            self.pub.publish(fwd)          	
+        elif msg.state == "NORMAL_JOYSTICK_CONTROL":
+            fwd.v = 0
+            fwd.omega = 0
+            self.flag = False
          
-    	  self.pub.publish(V)
+        self.pub.publish(fwd)
          
                  
 if __name__=='__main__':
-    rospy.init_node ('line', anonymous=true)
+    rospy.init_node('line', anonymous=True)
     ShapeNode()
-    
     rospy.spin()
