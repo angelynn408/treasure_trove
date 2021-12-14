d4e54b57fb8ea8980c271856ae79a51a2880a258
amanda <Amanda_Taylor1@student.uml.edu>
2021-12-14 18:39:58 -0500

    part2

--- packages/lab_2/src/part2.py
+++ packages/lab_2/src/part2.py
@@ -8,7 +8,7 @@ from duckietown_msgs.msg import WheelEncoderStamped, Pose2DStamped
 class OdomNode:
     def __init__(self):
         self.pose = Pose2DStamped()
-        self.R = rospy.Publisher("pose", Pose2DStamped, queue_size=10)
+        self.R = rospy.Publisher("velocity_to_pose_node/pose", Pose2DStamped, queue_size=10)
         self.x = 0
         self.y = 0
         self.theta = 0
@@ -21,26 +21,26 @@ class OdomNode:
         radius = .065/2
         self.circumference = radius*2*numpy.pi
         distance = .010
-        self.tick_per_rev = 135
+        self.rev_per_tick = 105
           
         self.left_tick = rospy.Subscriber("left_wheel_encoder_node/tick", WheelEncoderStamped, self.Left_Wheel)
         self.right_tick = rospy.Subscriber("right_wheel_encoder_node/tick", WheelEncoderStamped, self.Right_Wheel)
           
     def Left_Wheel(self, msg):
         #number of revolutions
-        revs_left = msg.data/ tick_per_rev
+        revs_left = msg.data/self.rev_per_tick
         #calculation of distance based on above
-        dist_left = circumference*revs_left
+        dist_left = self.circumference*revs_left
         #delta distance new - s
-        self.dist_wheel_left = delta_s_l + dist_left
+        self.dist_wheel_left = dist_left-self.delta_s_l
         
     
     def Right_Wheel(self, msg):
         #number of revolutions
-        revs_right = msg.data/ tick_per_rev
+        revs_right = msg.data/self.rev_per_tick
         #calculation of distance based on above
-        dist_right = circumference*revs_right
-        self.dist_wheel_right = delta_s_l + dist_right
+        dist_right = self.circumference*revs_right
+        self.dist_wheel_right = dist_right-self.delta_s_l
     
     def callback_function(self, msg):
         delta_s_r = self.dist_wheel_right
@@ -52,7 +52,9 @@ class OdomNode:
         self.x = self.x+delta_x
         self.y = self.y+delta_y
         self.theta = self.theta+delta_theta                       
-        self.R.publish(self.x,self.y,self.theta) 
+        self.R.publish(self.x,self.y,self.theta)
+        final_dist = "Duckie Distance %s"%self.R()
+        rospy.loginfo(final_dist)
                            
 if __name__=='__main__':
     rospy.init_node ('odom_node')
