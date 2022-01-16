e87faa746a98e264b001b31364ddd5e652a497a8
amanda <Amanda_Taylor1@student.uml.edu>
2022-01-16 12:58:20 -0500

    ticks

--- packages/lab_2/src/part2.py
+++ packages/lab_2/src/part2.py
@@ -4,12 +4,11 @@ from math import radians, sin, cos, sqrt
 import numpy
 import rospy
 from duckietown_msgs.msg import WheelEncoderStamped, Pose2DStamped
-from geometry_msgs.msg import Vector3
 
 class OdomNode:
     def __init__(self):
         self.pose = Pose2DStamped()
-        self.R = rospy.Publisher("velocity_to_pose_node/pose", Pose2DStamped, queue_size=10)
+        self.R = rospy.Publisher("pose", Pose2DStamped, queue_size=10)
         self.x = 0
         self.y = 0
         self.theta = 0
@@ -17,7 +16,6 @@ class OdomNode:
         self.s_r = 0
         self.delta_s_r = 0
         self.delta_s_l = 0
-        self.delta_s = 0
         self.delta_theta = 0
         radius = .065/2
         self.circumference = radius*2*numpy.pi
@@ -26,6 +24,7 @@ class OdomNode:
         self.dist_wheel_right = 0  
         self.left_tick = rospy.Subscriber("left_wheel_encoder_node/tick", WheelEncoderStamped, self.Left_Wheel)
         self.right_tick = rospy.Subscriber("right_wheel_encoder_node/tick", WheelEncoderStamped, self.Right_Wheel)
+       
                 
     def Left_Wheel(self, msg):
         #number of revolutions
@@ -33,7 +32,8 @@ class OdomNode:
         #calculation of distance based on above
         dist_left = self.circumference*revs_left
         #delta distance new - s
-        self.dist_wheel_left = dist_left-self.delta_s_l
+        self.delta_s_l = dist_left-self.dist_wheel_left
+        self.dist_wheel_left = dist_left
         
     
     def Right_Wheel(self, msg):
@@ -41,30 +41,31 @@ class OdomNode:
         revs_right = msg.data/self.rev_per_tick
         #calculation of distance based on above
         dist_right = self.circumference*revs_right
-        self.dist_wheel_right = dist_right-self.delta_s_r
-        
+        self.delta_s_r = dist_right-self.dist_wheel_right
+        self.dist_wheel_right = dist_right
     
     def callback_function(self):
         L = .05
-        self.delta_s_r = self.dist_wheel_right
-        self.delta_s_l = self.dist_wheel_left
-        self.delta_s = (self.delta_s_r+self.delta_s_l)/2
-        self.delta_theta = (self.delta_s_r-self.delta_s_l)/(2*L)
-        delta_x = self.delta_s*cos(self.theta+self.delta_theta/2)
-        delta_y = self.delta_s*sin(self.theta+self.delta_theta/2)
+        delta_s_r = self.delta_s_r
+        delta_s_l = self.delta_s_l
+        delta_s = (delta_s_r+delta_s_l)/2
+        self.delta_theta = (delta_s_r-delta_s_l)/(2*L)
+        delta_x = delta_s*cos(self.theta+self.delta_theta/2)
+        delta_y = delta_s*sin(self.theta+self.delta_theta/2)
         self.x = self.x+delta_x
         self.y = self.y+delta_y
         self.theta = self.theta+self.delta_theta 
         self.pose.x = self.x
         self.pose.y = self.y
         self.pose.theta = self.theta                      
-        rospy.loginfo("distance is", self.pose)
-        self.R.pub.publish(self.pose)
+        self.R.publish(self.pose)
         
-                           
+                          
 if __name__=='__main__':
     rospy.init_node ('odom_node', anonymous=True)
     O = OdomNode()
-    O.callback_function()
-    rospy.spin()
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        O.callback_function()
+        rate.sleep()
     
