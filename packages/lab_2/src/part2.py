e56bc02a688b82b9429ae9d6ae0cb7326e014e4c
amanda <Amanda_Taylor1@student.uml.edu>
2022-04-28 15:44:38 -0400

    part2

--- packages/lab_2/src/part2.py
+++ packages/lab_2/src/part2.py
@@ -22,6 +22,10 @@ class OdomNode:
         self.rev_per_tick = 120
         self.dist_wheel_left = 0
         self.dist_wheel_right = 0  
+        self.left_first = True
+        self.right_first = True
+        self.left_flag_new = False
+        self.right_flag_new = False
         self.left_tick = rospy.Subscriber("left_wheel_encoder_node/tick", WheelEncoderStamped, self.Left_Wheel)
         self.right_tick = rospy.Subscriber("right_wheel_encoder_node/tick", WheelEncoderStamped, self.Right_Wheel)
        
@@ -31,9 +35,12 @@ class OdomNode:
         revs_left = msg.data/self.rev_per_tick
         #calculation of distance based on above
         dist_left = self.circumference*revs_left
-        #delta distance new - s
         self.delta_s_l = dist_left-self.dist_wheel_left
         self.dist_wheel_left = dist_left
+        if self.left_first == True:
+            self.left_first = False
+        else:
+            self.left_flag_new = True
         
     
     def Right_Wheel(self, msg):
@@ -43,23 +50,29 @@ class OdomNode:
         dist_right = self.circumference*revs_right
         self.delta_s_r = dist_right-self.dist_wheel_right
         self.dist_wheel_right = dist_right
+        if self.right_first == True:
+            self.right_first = False
+        else:
+            self.right_flag_new = True
     
     def callback_function(self):
-        L = .05
-        delta_s_r = self.delta_s_r
-        delta_s_l = self.delta_s_l
-        delta_s = (delta_s_r+delta_s_l)/2
-        self.delta_theta = (delta_s_r-delta_s_l)/(2*L)
-        delta_x = delta_s*cos(self.theta+self.delta_theta/2)
-        delta_y = delta_s*sin(self.theta+self.delta_theta/2)
-        self.x = self.x+delta_x
-        self.y = self.y+delta_y
-        self.theta = self.theta+self.delta_theta 
-        self.pose.x = self.x
-        self.pose.y = self.y
-        self.pose.theta = self.theta                      
-        self.R.publish(self.pose)
-        
+        if self.right_flag_new == True and self.left_flag_new == True:  
+            L = .05
+            delta_s_r = self.delta_s_r
+            delta_s_l = self.delta_s_l
+            delta_s = (delta_s_r+delta_s_l)/2
+            self.delta_theta = (delta_s_r-delta_s_l)/(2*L)
+            delta_x = delta_s*cos(self.theta+self.delta_theta/2)
+            delta_y = delta_s*sin(self.theta+self.delta_theta/2)
+            self.x = self.x+delta_x
+            self.y = self.y+delta_y
+            self.theta = self.theta+self.delta_theta 
+            self.pose.x = self.x
+            self.pose.y = self.y
+            self.pose.theta = self.theta                      
+            self.R.publish(self.pose)
+            self.right_flag_new = False
+            self.left_flag_new = False      
                           
 if __name__=='__main__':
     rospy.init_node ('odom_node', anonymous=True)
