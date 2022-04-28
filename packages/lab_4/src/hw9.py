f3d020ebcbb9634584cd636832a213308316fbc6
amanda <Amanda_Taylor1@student.uml.edu>
2022-04-27 20:19:26 -0400

    hw9

---
+++ packages/lab_4/src/hw9.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32
+import PIDControllerClass as PID
+
+class Establish:
+    def __init__(self):
+        rospy.set_param("controller_ready", "true")
+        rospy.set_param("graph_ready", "true")
+        kp = 1
+        ki = 0
+        kd = 0
+        self.K = [kp, ki, kd]
+        self.dt = 0.01
+        self.Distance_PID = PID.PID(self.K, self.dt)
+        
+        self.err = rospy.Subscriber("/error", Float32, self.control)
+        self.pub = rospy.Publisher("/control_input", Float32, queue_size = 10)
+        
+    def control(self, error):
+        rospy.login(error.data)
+        sumPID = self.Distance_PID.run(error.data)
+        self.pub.publish(sumPID)
+        
+if __name__ == "__main__":
+    rospy.init_mode("hw9", anonymous=True)
+    Establish()
+    rospy.spin()
+        
---
+++ packages/lab_4/src/hw9_2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32
+import PIDControllerClass as PID
+
+class PID:
+    def __init__(self, K, dt):
+        self.kp = K[0]
+        self.ki = K[1]
+        self.kd = K[2]
+        self.dt = dt
+        self.e_integrate = 0
+        self.e_prev = None
+        self.out = 0
+        
+    def run(self,error):
+        e = error
+        self.e_integrate += e
+        if self.e_prev == None:
+            e_dot = 0.0
+        else:
+            e_dot = (e - self.e_prev) / self.dt
+        P = self.kp * e
+        I = self.ki = self.e_integrate
+        D = self.kd = e_dot
+        self.e_prev = e
+        sumPID = P + I + D
+        return(sumPID)
+        
+if __name__ == "__main__":
+    rospy.init_node("PID", anonymous=True)
+    rospy.spin()
