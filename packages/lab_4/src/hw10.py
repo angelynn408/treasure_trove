1d90dc81b508fb33d4bc38afd8d3e2e57ab9e843
amanda <Amanda_Taylor1@student.uml.edu>
2022-04-28 12:35:47 -0400

    logwarn throughout

--- packages/lab_4/src/hw10.py
+++ packages/lab_4/src/hw10.py
@@ -12,7 +12,7 @@ def ServiceClient(n):
         fib = rospy.ServiceProxy('calc_fibonacci', Fibonacci)
         nth = fib(n)
         ServT2 = rospy.get_time()
-        rospy.loginfo("Service time is for " +str(n) +" request " +str(ServT2-ServT1))
+        rospy.logwarn("Service time for " +str(n) +" request is " +str(ServT2-ServT1))
         return nth.sequence
     except rospy.ServiceException as e:
         print("Service call failure %s" %e)
@@ -30,18 +30,18 @@ def ActionClient(n):
     ActT3 = rospy.get_time()
     
     Send_Time = ActT2 - ActT1
-    rospy.loginfo("Action" +str(n)+ "Send Time is " +str(Send_Time))
+    rospy.logwarn("Action for " +str(n)+ "Send Time is " +str(Send_Time))
     Wait_Time = ActT3 - ActT2 
-    rospy.loginfo("Action" +str(n)+ "Wait Time is " +str(Wait_Time))
+    rospy.logwarn("Action" +str(n)+ "Wait Time is " +str(Wait_Time))
     return client.get_result()
     
 if __name__=="__main__":
     rospy.init_node('hw10')
     Serv3 = ServiceClient(3)
-    rospy.loginfo("Service Order 3 is " +str(Serv3))
+    rospy.logwarn("Service Order 3 is " +str(Serv3))
     Serv15= ServiceClient(15)
-    rospy.loginfo("Service Order 15 is " +str(Serv15))
+    rospy.logwarn("Service Order 15 is " +str(Serv15))
     Act3 = ActionClient(3)
-    rospy.loginfo("Action order 3 is" +str(Act3))
+    rospy.logwarn("Action order 3 is" +str(Act3))
     Act15 = ActionClient(15)
-    rospy.loginfo("Action order 15 is" +str(Act15))
+    rospy.logwarn("Action order 15 is" +str(Act15))
