9b1a3d2a46319f30723f940ab88a3b7da3aac80c
amanda <Amanda_Taylor1@student.uml.edu>
2022-04-28 19:26:41 -0400

    kp down .02

--- packages/lab_4/src/hw9.py
+++ packages/lab_4/src/hw9.py
@@ -8,8 +8,8 @@ class Establish:
     def __init__(self):
         rospy.set_param("controller_ready", "true")
         rospy.set_param("graph_ready", "true")
-        kp = .49
-        ki = 0.00002
+        kp = .47
+        ki = 0.00001
         kd = 0.03
         self.K = [kp, ki, kd]
         self.dt = 0.01
