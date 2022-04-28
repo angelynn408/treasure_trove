78ef57a8d7f66cb64ffaed41368dd407b664c149
amanda <Amanda_Taylor1@student.uml.edu>
2022-04-28 19:39:52 -0400

    test

--- packages/lab_4/src/hw9.py
+++ packages/lab_4/src/hw9.py
@@ -9,8 +9,8 @@ class Establish:
         rospy.set_param("controller_ready", "true")
         rospy.set_param("graph_ready", "true")
         kp = 0.47
-        ki = 0.0
-        kd = 0.0
+        ki = 0.00003
+        kd = 0.05
         self.K = [kp, ki, kd]
         self.dt = 0.01
         self.Distance_PID = PID.PID(self.K, self.dt)
